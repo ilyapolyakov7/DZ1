0cfc861b864c55508b4d2c1c6804689fd14e23d5
ilya polyakov <mail@mail.ru>
2022-10-05 21:29:11 +0300

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое целое число");
+int user_number = int.Parse(Console.ReadLine());
+int x = 1;
+
+Console.WriteLine("Все четные числа от 1 до " + user_number + ":");
+while (x < user_number)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine(x);
+    }
+    x++;
+}

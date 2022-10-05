6cee69845445ed60e0f1f33ad3906ad7a1644946
ilya polyakov <mail@mail.ru>
2022-10-05 20:16:23 +0300

    zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 2 любых целых числа");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}

96669f7b33db71c09eb12f6f53fd4ab259c36779
ilya polyakov <mail@mail.ru>
2022-10-05 20:54:23 +0300

    Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 3 любых целых числа");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+
+if (numberB > max)
+{
+        max = numberB;
+}
+if (numberC > max)
+{
+        max = numberC;
+}
+Console.WriteLine("max = " +max);
